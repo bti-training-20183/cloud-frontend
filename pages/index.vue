51b82c31afe6a8a4d5f65737af6ddaba82eb7cdb
Homuru <quangdt2510@gmail.com>
2019-07-08 18:47:47 +0700

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <div class="text-xs-center">
+        <upload-form />
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+import VuetifyLogo from "~/components/VuetifyLogo.vue";
+import UploadForm from "~/components/UploadForm.vue";
+export default {
+  components: {
+    Logo,
+    VuetifyLogo,
+    UploadForm
+  }
+};
+</script>
---
+++ pages/test/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="upload">
+    <input type="file" @change="onFileSelected" accept=".csv,text/plain"/>
+    <v-btn color="info" @click="onUpload">Upload</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedFile: null,
+      progress: null
+    };
+  },
+
+  methods: {
+    onFileSelected(event) {
+      this.selectedFile = event.target.files[0];
+      console.log(this.selectedFile);
+    },
+    onUpload() {
+      const fd = new FormData();
+      fd.append("data", this.selectedFile, this.selectedFile.name);
+      console.log(fd);
+    }
+  }
+};
+</script>
+
